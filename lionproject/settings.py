690db13f1d112d4636ffa425bd7fb56ba22993b9
seunghanhong <hshb5815@naver.com>
2021-05-11 00:06:02 +0900

    edit database

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'lionproject.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': os.environ.get('DB_NAME'),
+       'USER': os.environ.get('DB_USER'),
+       'PASSWORD': os.environ.get('DB_PASSWORD'),
+       'HOST': os.environ.get('DB_HOST'),
+       'PORT': '',
+   }
 }
 
 
